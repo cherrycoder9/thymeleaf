7a6601ac644b4de268738547a88359ccdb52af85
cherrycoder9 <cherrycoder9@gmail.com>
2024-10-09 19:56:06 +0900

    ...

---
+++ src/main/java/hacker/thymeleaf/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hacker.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(final Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
