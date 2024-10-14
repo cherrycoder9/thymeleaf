99a3843aee4cc0741753c622ac0d08fab3d97ccc
cherrycoder9 <cherrycoder9@gmail.com>
2024-10-14 10:58:55 +0900

    ...

---
+++ src/main/java/hacker/thymeleaf/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hacker.thymeleaf.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
